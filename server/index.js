acb9a4b65f370895e2b112fe5c36e57e8639e70a
Jaee-C <77240633+Jaee-C@users.noreply.github.com>
2021-09-18 14:26:34 +0800

    query for actions

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const url = require('url');
 const app = express();
 
 const db = require('./models');
 
-const { Items } = require("./models");
+const { Items, Actions } = require("./models");
 
 const cors = require('cors');
+const ItemActions = require('./models/ItemActions');
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
@@ -13,6 +15,22 @@ app.use(cors({
 app.use(express.json());
 
 app.get('/api/items', async (req, res) => {
+    const q = url.parse(req.url, true);
+    const qdata = q.query;
+
+    if (qdata.id) {
+        const action = Actions.findAll({
+            attributes: ['actionName', 'description'],
+            where: {ItemId: qdata.id},
+            include: [{
+                model: ItemActions,
+                required: true
+            }]
+        });
+        console.log(qdata.id);
+        res.json(action);
+    }
+
     const listOfItem = await Items.findAll();
     res.json(listOfItem);
 })
@@ -22,6 +40,10 @@ app.post('/api/items', async (req, res) => {
     await Items.create(item);
     res.json(item);
 });
+
+app.get('/api/items', (req, res) => {
+    
+})
 
 db.sequelize.sync().then(() => {
     app.listen(3001, () => {
