fdb7cd71c7756d72e0bd6802b889d3d8a3ac5ec6
Jaee-C <wengjae.1011@gmail.com>
2021-09-18 09:21:03 +0800

    comments

--- server/index.js
+++ server/index.js
@@ -3,10 +3,11 @@ const app = express();
 
 const db = require('./models');
 
-// Routers
+// Everything below this line is useless
 const cardRouter = require('./routes/Card')
 app.use("/cards", cardRouter);
 
+// This is to check if it's working
 db.sequelize.sync().then(() => {
     app.listen(3001, () => {
         console.log("Server running on port 3001");
